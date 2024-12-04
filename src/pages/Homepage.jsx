7c5d170b7fb1d652a7bfb0004f9e6e85b7c1dc14
ksenia <kseniapet2005@mail.ru>
2024-12-04 08:01:17 +0300

    add head in MainPage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,9 +1,17 @@
-function Homepage(){
-    
-    return(
-        <div>
-            Homepage
+import React from "react";
+import headImg from "../assets/home-head.jpg";
+
+
+function Homepage() {
+  return (
+    <div className="head">
+        <img src={headImg} alt="Gardening" className="head-image" />
+        <div className="head-content">
+            <h1>Amazing Discounts <br /> on Garden Products!</h1>
+            <button className="head-button">Check out</button>
         </div>
-    )
+    </div>
+  );
 }
-export default Homepage
+
+export default Homepage;
